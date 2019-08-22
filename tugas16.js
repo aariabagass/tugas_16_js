249d54b32aea06925233ccfeff420ae54c501ec3
Aria Bagas <aariabagass@gmail.com>
2019-08-22 23:33:24 +0700

    JS Task 16
    min max

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+var minMax = () => {
+  min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  console.log('Nilai Minimal : ' + min);
+  console.log('Nilai Maximal : ' + max);
+  return minMax;
+}
+minMax();
